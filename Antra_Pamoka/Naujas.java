04a83235c942a6127b7ca2f5953a47dccb1cff33
JustPause <Justinaslolt@gmail.com>
2023-04-26 21:50:10 +0300

    pamoka 2

---
+++ Antra_Pamoka/Naujas.java
@@ -0,0 +1,49 @@
+package Antra_Pamoka;
+
+class Katinas {
+    String Varas;
+    int Amius;
+    boolean Skiepitas;
+
+}
+
+public class Naujas {
+
+    public static void Sujunktas() {
+        String Vardads = "Justinas";
+        String Pavarde = "Stan";
+
+        String Sudetis = Vardads + Pavarde;
+        String Sutrumpimamia = Sudetis.substring(3, 7);
+        System.out.println(Sutrumpimamia);
+    }
+
+    public static void koreja() {
+        String angl = "Hello";
+        String kol = null;
+
+        kol = "annyeonghaseyo";
+
+        boolean arStingas = kol instanceof String;
+
+        System.out.println(kol.length());
+        System.out.println(kol.indexOf('y'));
+    }
+
+    public static void katinas() {
+        Katinas Pirmasis = new Katinas();
+
+        Pirmasis.Varas = "Vytautas";
+        Pirmasis.Amius = 7;
+        Pirmasis.Skiepitas = false;
+
+        System.out.printf("Vardas: %s\nAmžius: %d\nAr Skiepitas: %b\n", Pirmasis.Varas, Pirmasis.Amius,
+                Pirmasis.Skiepitas);
+
+        System.out.print(Pirmasis);
+    }
+
+    public static void main(String[] args) {
+        Sujunktas();
+    }
+}
---
+++ Antra_Pamoka/w3schools.java
@@ -0,0 +1,19 @@
+package Antra_Pamoka;
+
+class Main {
+    int modelYear;
+    String modelName;
+  
+    public Main(int year, String name) {
+      modelYear = year;
+      modelName = name;
+    }
+}
+
+public class w3schools 
+{
+    public static void main(String[] args) {
+        Main myCar = new Main(1969, "Mustang");
+        System.out.println(myCar.modelYear + " " + myCar.modelName);
+    }
+}
