3f9ddb71ac3ac9d17e1a60995cb0e5aa3b18c43e
JustPause <Justinaslolt@gmail.com>
2023-05-03 21:06:06 +0300

    Up

--- Ketvirta_Pamoka/Uzduotis.java
+++ Ketvirta_Pamoka/Uzduotis.java
@@ -132,7 +132,7 @@ public class Uzduotis {
     }
 
     static public void UzPamoka4() {
-        Map<Integer, String> planetos = new HashMap<Integer, String> ();
+        Map<Integer, String> planetos = new HashMap<Integer, String>();
         planetos.put(3, "Zeme");
         planetos.put(1, "Merkurijus");
         planetos.put(4, "Marsas");
@@ -141,20 +141,25 @@ public class Uzduotis {
         planetos.put(7, "Saturnas");
         planetos.put(6, "Menulis");
 
-
         System.out.println(planetos);
         System.out.println(planetos.get(8));
 
         boolean a = planetos.containsKey(6);
         boolean b = planetos.containsKey("Merkurijus");
 
-        if (a){
+        if (a) {
             planetos.remove(6);
         }
         System.out.println(planetos);
     }
+
     static public void UzPamoka5() {
-        Map<Integer, String> planetos = new HashMap<Integer, String> ();
+        List<String> pirmieji = new ArrayList<String>();
+
+        String[] Vardai = { "Justinas", "Vilius", "Mindaugas", "Rokas" };
+        List<String> MokiniuVardai = Arrays.asList(Vardai);
+
+        Map<Integer, String> planetos = new HashMap<Integer, String>();
         planetos.put(3, "Zeme");
         planetos.put(1, "Merkurijus");
         planetos.put(4, "Marsas");
@@ -163,17 +168,54 @@ public class Uzduotis {
         planetos.put(7, "Saturnas");
         planetos.put(6, "Menulis");
 
+        String[] Data = { "Du", "gaideliai", "du", "gaideliai", "baltus", "žirnius", "kūlė" };
 
-        System.out.println(planetos);
-        System.out.println(planetos.get(8));
+        pirmieji.add(MokiniuVardai.get(0));
+        pirmieji.add(planetos.get(1));
 
-        boolean a = planetos.containsKey(6);
-        boolean b = planetos.containsKey("Merkurijus");
+        // List<String> LisISSeto = new ArrayList<String>(Data);
+    }
 
-        if (a){
-            planetos.remove(6);
+    static public void UzPamoka6() {
+        class Mokinys {
+            String Varas;
+            int pazinius[];
+            float vidurkis;
+
         }
-        System.out.println(planetos);
+        Mokinys var1 = new Mokinys();
+        var1.Varas = "Maryte Kavalyte";
+        var1.pazinius = new int[] { 1, 3, 4 };
+        var1.vidurkis = 0;
+
+        for (int i = 0; i < var1.pazinius.length; i++) {
+            var1.vidurkis += var1.pazinius[i];
+        }
+
+        var1.vidurkis = var1.vidurkis / var1.pazinius.length;
+
+        Mokinys var2 = new Mokinys();
+        var2.Varas = "Dede Zede";
+        var2.pazinius = new int[] { 9, 8, 10 };
+        var2.vidurkis = 0;
+
+        for (int i = 0; i < var2.pazinius.length; i++) {
+            var2.vidurkis += var2.pazinius[i];
+        }
+
+        var2.vidurkis = var2.vidurkis / var2.pazinius.length;
+
+        Mokinys var3 = new Mokinys();
+        var3.Varas = "Zozea Dose";
+        var3.pazinius = new int[] { 2, 4, 9 };
+        var3.vidurkis = 0;
+
+        for (int i = 0; i < var3.pazinius.length; i++) {
+            var3.vidurkis += var3.pazinius[i];
+        }
+
+        var3.vidurkis = var3.vidurkis / var3.pazinius.length;
+
     }
 
     public static void main(String[] args) {
