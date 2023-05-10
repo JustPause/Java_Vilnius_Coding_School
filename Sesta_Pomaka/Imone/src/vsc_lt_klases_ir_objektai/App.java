1f650c1161ca5680f41320fb559dad8ae607a6fd
JustPause <Justinaslolt@gmail.com>
2023-05-10 15:57:11 +0300

    nuoj

---
+++ Sesta_Pomaka/Imone/src/vsc_lt_klases_ir_objektai/App.java
@@ -0,0 +1,10 @@
+package vsc_lt_klases_ir_objektai;
+
+public class App {
+    public static void main(final String[] args) throws Exception {
+        final Darbuotojas darb1 = new Darbuotojas("Justinas", "Stankunas", "62407068357");
+        System.out.println(darb1.vardaString());
+        final Imone im = new Imone("Ymone", 2020, "Bulvytes 13g.");
+        
+    }
+}
