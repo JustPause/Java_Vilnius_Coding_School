1d386c1e3a22294d38481affe6613ea46bfcd063
JustPause <Justinaslolt@gmail.com>
2023-05-15 18:43:48 +0300

    Update

--- Septinta_Pamoka/ND_encryption/encryption/src/App.java
+++ Septinta_Pamoka/ND_encryption/encryption/src/App.java
@@ -1,8 +1,7 @@
-public class Camellia {
+public class App {
     public static void main(String[] args) throws Exception {
         Work work = new Work(0);
         work.input_metod();
         work.print();
     }
 }
-d
