e969d7d581ec6d4249d0224be11c410d8c6a3ee6
Justinas Stankunas (JustPuase) <IamJustStan@hotmail.com>
2023-06-15 02:03:50 +0300

    B

--- 2_NaujasLektorius/SpringBoot/src/main/java/lt/justinas/springdemo/BootStrap/BootStrapData.java
+++ 2_NaujasLektorius/SpringBoot/src/main/java/lt/justinas/springdemo/BootStrap/BootStrapData.java
@@ -1,2 +1,24 @@
-package lt.justinas.springdemo.BootStrap;public class BootStrapData {
+package lt.justinas.springdemo.BootStrap;
+
+import lt.justinas.springdemo.repository.CustumerRepo;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+	private CustumerRepo custumerRepo;
+
+	public BootStrapData(CustumerRepo custumerRepo) {
+		this.custumerRepo = custumerRepo;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		custumerRepo.findAll().forEach(System.out::println);
+		System.out.println("\n----\n");
+		System.out.println(
+				custumerRepo.findById(161)
+		);
+	}
 }
--- 2_NaujasLektorius/SpringBoot/src/main/java/lt/justinas/springdemo/repository/CustumerRepo.java
+++ 2_NaujasLektorius/SpringBoot/src/main/java/lt/justinas/springdemo/repository/CustumerRepo.java
@@ -1,2 +1,8 @@
-package lt.justinas.springdemo.repository;public interface CustumerRepo {
+package lt.justinas.springdemo.repository;
+
+import lt.justinas.springdemo.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustumerRepo extends CrudRepository<Customer,Integer> {
+
 }
