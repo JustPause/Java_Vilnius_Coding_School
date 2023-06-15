669341d4789fbd9666180e06a58c07ddaf7c7014
Justinas Stankunas (JustPuase) <IamJustStan@hotmail.com>
2023-06-15 19:06:47 +0300

    Add lonmg

--- 2_NaujasLektorius/SpringBoot/src/main/java/lt/justinas/springdemo/BootStrap/BootStrapData.java
+++ 2_NaujasLektorius/SpringBoot/src/main/java/lt/justinas/springdemo/BootStrap/BootStrapData.java
@@ -1,17 +1,15 @@
 package lt.justinas.springdemo.BootStrap;
 
 import lt.justinas.springdemo.repository.CustumerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 
+	@Autowired
 	private CustumerRepo custumerRepo;
-
-	public BootStrapData(CustumerRepo custumerRepo) {
-		this.custumerRepo = custumerRepo;
-	}
 
 	@Override
 	public void run(String... args) throws Exception {
