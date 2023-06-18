ae711f682a8350943a93bc493f5212c0c24dc846
JustPause <Justinaslolt@gmail.com>
2023-06-19 02:38:13 +0300

    idk

--- Finall_Work/BDayGetter/app/src/main/java/com/example/b_daygetter/MainActivity.java
+++ Finall_Work/BDayGetter/app/src/main/java/com/example/b_daygetter/MainActivity.java
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        R.id.MainText.
     }
 }
