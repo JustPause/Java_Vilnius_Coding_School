2569fa2b9de9dadf3b7f21c05e3e9fe492de835d
Justinas Stankunas (JustPuase) <IamJustStan@hotmail.com>
2023-06-26 22:18:36 +0300

    Yes

---
+++ 2_NaujasLektorius/MyNotes/app/build/generated/source/buildConfig/debug/lt/justinas/mynotes/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package lt.justinas.mynotes;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "lt.justinas.mynotes";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
